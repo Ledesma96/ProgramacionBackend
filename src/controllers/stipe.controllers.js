51a45a5360e10811bec4884bf26cef0ed0684bf5
gabriel <gabriel.m.ledesma96@gmail.com>
2023-12-13 12:42:49 -0300

    production

--- src/controllers/stipe.controllers.js
+++ src/controllers/stipe.controllers.js
@@ -2,7 +2,18 @@ import Stripe from "stripe"
 import 'dotenv/config.js'
 import cartModel from "../Dao/mongo/models/cart.model.js"
 
-const stripe = new Stripe('sk_test_51OHnm5DZBMmoG22QqcpX9I4ix1335WxJbvPtnhhG2uiuL1E1xhNb5sNmqnDg0KBsHMBnQcVRZM2DJyt8Z3UVCwnW00fupHv7BJ')
+const stripe = new Stripe(process.env.PRIVATE_KEY_STRIPE)
+
+function buildUrl(path) {
+  if (process.env.NODE_ENV === 'production') {
+    
+    return `https://programacionbackend-production-a757.up.railway.app${path}`;
+  } else {
+   
+    return `http://localhost:8080${path}`;
+  }
+}
+
 
 export const StripePayment = async (req, res) => {
     const cartId = req.body.cart
@@ -24,8 +35,8 @@ export const StripePayment = async (req, res) => {
         line_items: lineItems,
         payment_method_types: ['card'],
         mode: 'payment',
-        success_url: '/',
-        cancel_url: '/error'
+        success_url: buildUrl('/'),
+        cancel_url: buildUrl('/error')
     });
 
     return res.json(session);
